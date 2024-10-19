8209f2d19f0985d79c59fdaf4ae505616b058049
EYoung21 <eliyoung4now@gmail.com>
2024-10-19 08:06:24 -0400

    c

--- oa_processor.py
+++ oa_processor.py
@@ -1,6 +1,6 @@
 # How to build exe if don't already have it
 # 1. You must have pytesseract (https://github.com/UB-Mannheim/tesseract/wiki) downlaoded and located in the default folder: C:\Program Files\Tesseract-OCR\tesseract.exe
-# 2. run: pyinstaller oa_processor.spec
+# 2. run: "pyinstaller oa_processor.spec" in the terminal
 # 3. right click the "dist" folder
 # 4. click "reveal in file explorer"
 # 5. open the dist folder, and copy the exe
